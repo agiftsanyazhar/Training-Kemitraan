d6b5060cc67ea82c89922aa3169292f95ea8fa10
HerrysAghista <herrys.aghista.rachman-2020@vokasi.unair.ac.id>
2022-05-30 21:41:45 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('Admin', function(User $user){         
+            return $user->role === 'Admin';
+        });
+        Gate::define('Principal', function(User $user){         
+            return $user->role === 'Principal';
+        });
+        Gate::define('Distributor', function(User $user){         
+            return $user->role === 'Distributor';
+        });
+        Gate::define('Agen', function(User $user){         
+            return $user->role === 'Agen';
+        });
+        Gate::define('Reseller', function(User $user){         
+            return $user->role === 'Reseller';
+        });
+        Gate::define('Dropshipper', function(User $user){         
+            return $user->role === 'Dropshipper';
+        });
     }
 }
