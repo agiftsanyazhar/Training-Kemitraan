f8479bea2cf4c67e79ee8cc842aae382a4e28454
BintangAchmadM <bintang.achmad.mudzakie-2020@vokasi.unair.ac.id>
2022-06-02 11:30:49 +0700

    pussy

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -377,7 +377,7 @@ Function: require_once
                 
                     
 
-                    <tr>
+                    {{--  <tr>
                         <td class="align-top border-right">
                             <div class="d-flex">
                                 <div class="p-2 pt-0 align-self-center">
@@ -541,7 +541,7 @@ Function: require_once
                                 <a href="https://dashboard.sellerkit.id/product/edit/25" class="text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg></a>
                                 <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#ModalDelete25" class="text-danger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
                             </td>
-                                                        </tr>
+                                                        </tr>  --}}
                                         </tbody>
         </table>
     </div>
