849e244bd1db8fe9c1c4c10d5ebe0d5c77a260fa
BintangAchmadM <bintang.achmad.mudzakie-2020@vokasi.unair.ac.id>
2022-06-02 12:18:42 +0700

    commit lagi 02/06

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/', [IndexController::class, 'index'])->middleware('auth');
 
-Route::get('/dashboard', [IndexController::class, 'index'])->middleware('auth');
+Route::get('/', [IndexController::class, 'index'])->middleware('auth');
 Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
 Route::get('/charts', [KeranjangController::class, 'index'])->middleware('auth');
 Route::get('/stock', [StockController::class, 'index'])->middleware('auth');
