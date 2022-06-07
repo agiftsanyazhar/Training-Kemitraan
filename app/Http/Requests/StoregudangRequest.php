85054344a2ad0ebc1e34874a3e9734bfe045b249
HerrysAghista <herrys.aghista.rachman-2020@vokasi.unair.ac.id>
2022-06-07 20:39:13 +0700

    permit ga jelas

--- app/Http/Requests/StoregudangRequest.php
+++ app/Http/Requests/StoregudangRequest.php
@@ -13,7 +13,7 @@ class StoregudangRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/StorekategoriRequest.php
+++ app/Http/Requests/StorekategoriRequest.php
@@ -13,7 +13,7 @@ class StorekategoriRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
