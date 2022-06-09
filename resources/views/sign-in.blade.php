1599a804a06c76f99c69268d5883d72ba8edfbc2
Agiftsany <agiftsanyazhar@gmail.com>
2022-06-09 15:17:13 +0700

    push

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.signin-signup')
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link rel="canonical" href="https://demo-basic.adminkit.io/pages-sign-in" />
-
-	<title>Sign In | AdminKit Demo</title>
-
-	<link href="css/app.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-</head>
-
-<body>
+@section('container')
 	<main class="d-flex w-100">
 		<div class="container d-flex flex-column">
 			<div class="row vh-100">
@@ -53,13 +33,13 @@
 										@csrf
 										<div class="mb-3">
 											<label class="form-label">Username</label>
-											<input class="form-control form-control-lg" type="text" name="username" placeholder="Type username" minlength="3" maxlength="50" required/>
+											<input class="form-control form-control-lg" type="text" name="username" placeholder="Masukkan username" minlength="3" maxlength="50" required/>
 										</div>
 										<div class="mb-3">
 											<label class="form-label">Password</label>
-											<input class="form-control form-control-lg" type="password" name="password" placeholder="Type password" minlength="8" maxlength="32" required/>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Masukkan password" minlength="8" maxlength="32" required/>
 											<small>
-												<a href="{{ url ('/lupa-password') }}">Forgot password?</a>
+												<a href="{{ url ('/lupa-password') }}">Lupa password?</a>
 											</small>
 										</div>
 										
@@ -67,16 +47,13 @@
 											<label class="form-check">
 												<input class="form-check-input" type="checkbox" value="remember-me" name="remember-me" checked>
 												<span class="form-check-label">
-												remember me
+												Ingat saya
 												</span>
 											</label>
 										</div>
 										<div class="text-center d-grid gap-2 mt-2 mb-3">
 											<button type="submit" class="btn btn-lg btn-primary">Sign in</button>
 										</div>
-										<small>
-											New User ? <a href="{{ url ('/sign-up') }}">Sign up!</a>
-										</small>
 									</form>
 								</div>
 							</div>
@@ -86,10 +63,5 @@
 				</div>
 			</div>
 		</div>
-	</main>
-
-	<script src="js/app.js"></script>
-
-</body>
-
-</html>
+	</main>	
+@endsection
