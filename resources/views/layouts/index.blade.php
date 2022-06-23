9f9f987ace5ddcc23c8868403598011ca938d9cd
HerrysAghista <herrys.aghista.rachman-2020@vokasi.unair.ac.id>
2022-06-23 12:40:17 +0700

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -17,7 +17,7 @@
 	<script src="/js/jquery-3.6.0.js"></script>
 
 	<title>Sellerkit</title>
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link href="css/app.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 
